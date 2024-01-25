923f731c901adde0b7f4ce7567c64d5b7f05ecd7
maxiamikel <maxloversist@gmail.com>
2024-01-25 13:42:28 -0300

    First commit

---
+++ src/main/java/com/maxi/backstore/BackStoreApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.backstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxi/backstore/BackStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.backstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
